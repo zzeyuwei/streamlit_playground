08c127e85c0c965ba4b1518932b4717360d7e3fd
Shanshan Xu <shanshan.xu@tum.de>
2023-09-15 16:40:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,25 +16,25 @@ def generate_response(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
     if uploaded_file is not None:
         pages = []
-    	pdf = pypdf.PdfReader(file)
-    	for p in range(len(pdf.pages)):
-    		page = pdf.pages[p]
-    		text = page.extract_text()
-    		pages += [text]
+        pdf = pypdf.PdfReader(file)
+        for p in range(len(pdf.pages)):
+            page = pdf.pages[p]
+            text = page.extract_text()
+            pages += [text]
 
-        # documents = loader.load()
-        # Split documents into chunks
-        text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-        texts = text_splitter.create_documents(documents)
-        # Select embeddings
-        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-        # Create a vectorstore from documents
-        db = Chroma.from_documents(texts, embeddings)
-        # Create retriever interface
-        retriever = db.as_retriever()
-        # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-        return qa.run(query_text)
+    # documents = loader.load()
+    # Split documents into chunks
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    texts = text_splitter.create_documents(documents)
+    # Select embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    # Create a vectorstore from documents
+    db = Chroma.from_documents(texts, embeddings)
+    # Create retriever interface
+    retriever = db.as_retriever()
+    # Create QA chain
+    qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+    return qa.run(query_text)
 
 # Page title
 st.set_page_config(page_title='🦜🔗 Ask the Doc App')
