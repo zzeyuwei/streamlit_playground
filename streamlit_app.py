52cb2c7f9d57d6ac50ed9ce9df9baeb4ef7959ca
Shanshan Xu <shanshan.xu@tum.de>
2023-11-05 16:13:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,55 +3,21 @@ import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from langchain.llms import OpenAI
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
-import pypdf
 
-def generate_response(uploaded_file, openai_api_key, query_text):
-    # Load document if file is uploaded
-    if uploaded_file is not None:
-        documents = []
-        pdf = pypdf.PdfReader(uploaded_file)
-        for p in range(len(pdf.pages)):
-            page = pdf.pages[p]
-            text = page.extract_text()
-            documents += [text]
+### customize your title here ###
+st.title('🦜🔗 First Chat')
 
-    # documents = loader.load()
-    # Split documents into chunks
-    text_splitter=CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.create_documents(documents)
-    # Select embeddings
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    # Create a vectorstore from documents
-    db = Chroma.from_documents(texts, embeddings)
-    # Create retriever interface
-    retriever = db.as_retriever()
-    # Create QA chain
-    qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-    return qa.run(query_text)
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
-# Page title
-st.set_page_config(page_title='🦜🔗 Ask the Doc App')
-st.title('🦜🔗 Ask the Doc App')
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
 
-# File upload
-uploaded_file = st.file_uploader('Upload an article', type='pdf')
-# Query text
-query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
-
-# Form input and query
-result = []
-with st.form('myform', clear_on_submit=True):
-    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
-    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
+with st.form('my_form'):
+    ### customize your text area here ###
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
     if submitted and openai_api_key.startswith('sk-'):
-        with st.spinner('Calculating...'):
-            response = generate_response(uploaded_file, openai_api_key, query_text)
-            result.append(response)
-            del openai_api_key
-
-if len(result):
-    st.info(response)
+        generate_response(text)
