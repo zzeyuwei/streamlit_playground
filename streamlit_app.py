858f56cec5bd67a3cc9494aef0ed71b13dcd3383
zzeyuwei <150336631+zzeyuwei@users.noreply.github.com>
2023-11-09 11:28:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,47 @@
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
-from langchain.llms import OpenAI
+from langchain import OpenAI
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
 
-### customize your title here ###
-st.title('🦜🔗 My first App')
 
-# openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
-def generate_response(input_text):
-    ### customize temperature between 0 to 1 ####
-    # Higher temperatures introduce randomness/diverse output, which is beneficial for creative tasks.
-    # In contrast, a temperature of zero ensures consistent/deterministic responses,
-    # making GPT a reliable tool for obtaining determined outputs with no variation.
-    ###
 
+
+def generate_response(txt):
+    # Instantiate the LLM model
     llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+    # Split text
+    text_splitter = CharacterTextSplitter()
+    texts = text_splitter.split_text(txt)
 
-with st.form('my_form'):
-    ### customize your text area here ###
-    openai_api_key = st.text_input('OpenAI API Key', type='password')
-    txt_input = st.text_area('Enter text:', 'enter your text here')
+    # Create multiple documents
+    docs = [Document(page_content=t) for t in texts]
+    # Text summarization
+    chain = load_summarize_chain(llm, chain_type='map_reduce')
+    return chain.run(docs)
 
+# Page title
+st.set_page_config(page_title='🦜🔗 Text Summarization App')
+st.title('🦜🔗 Text Summarization App')
+
+# Text input
+txt_input = st.text_area('Enter your text', '', height=200)
+
+# Form to accept user's text input for summarization
+result = []
+with st.form('summarize_form', clear_on_submit=True):
+    openai_api_key = st.text_input('OpenAI API Key', type = 'password', disabled=not txt_input)
     submitted = st.form_submit_button('Submit')
-    if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
     if submitted and openai_api_key.startswith('sk-'):
-        generate_response(txt_input)
+        with st.spinner('Calculating...'):
+            response = generate_response(txt_input)
+            result.append(response)
+            del openai_api_key
+
+if len(result):
+    st.info(response)
